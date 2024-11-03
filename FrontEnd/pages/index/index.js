283e07f1506058d3625504f85ff8a10ac458bad8
DBWGLX <1125506557@qq.com>
2024-11-03 14:07:01 +0800

    Edited the home page

--- FrontEnd/pages/index/index.js
+++ FrontEnd/pages/index/index.js
@@ -1,25 +1,32 @@
 Page({
   data: {
-    originalList: [
-      { id: 1, name: "张三", class: "2020届" },
-      { id: 2, name: "李四", class: "2019届" },
-      { id: 3, name: "马化腾", class: "1989届"}
-    ],
-    alumniList: [],
-    searchQuery: ''
+    newsList: [
+      { 
+        id: 1, 
+        title: "校友聚会活动通知", 
+        content: "我们将举行一场校友聚会，期待各位校友的参与！", // 新增内容
+        date: "2024-11-01", 
+        views: 120, // 新增阅读量
+        image: "../../image/news_image1.jpg" // 新增图片路径
+      },
+      { 
+        id: 2, 
+        title: "最新校友新闻", 
+        content: "校友会最近举办了一次成功的募捐活动，感谢大家的支持！", // 新增内容
+        date: "2024-10-28", 
+        views: 85, // 新增阅读量
+        image: "../../image/news_image2.jpg" // 新增图片路径
+      },
+      { 
+        id: 3, 
+        title: "校友募捐活动成功", 
+        content: "我们成功筹集到足够的资金，用于支持在校学生。", // 新增内容
+        date: "2024-10-25", 
+        views: 45, // 新增阅读量
+        image: "../../image/news_image3.jpg" // 新增图片路径
+      }
+    ]
   },
-  onLoad() {
-    this.setData({ alumniList: this.data.originalList }); // 初始化时显示所有校友
-  },
-  onSearch(e) {
-    const query = e.detail.value;
-    this.setData({ searchQuery: query });
-    const filteredList = this.data.originalList.filter(item => 
-      item.name.toLowerCase().includes(query.toLowerCase()) // 过滤校友列表
-    );
-    this.setData({ alumniList: filteredList }); // 更新视图
-    console.log(filteredList);
-    },
   goToDetail(e) {
     const id = e.currentTarget.dataset.id;
     wx.navigateTo({
