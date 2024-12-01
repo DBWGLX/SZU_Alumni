7e92c6809f162f62c8dd7f27441b5b2057e1e844
Lian paifeng <151843929+Lian314@users.noreply.github.com>
2024-12-02 00:24:53 +0800

    Delete BackEnd/spring-boot-data-H2-embedded directory

--- BackEnd/spring-boot-data-H2-embedded/src/main/java/com/iamvickyav/springboot/SpringBootRestWithH2/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.iamvickyav.springboot.SpringBootRestWithH2.service;
-
-import com.iamvickyav.springboot.SpringBootRestWithH2.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserService extends JpaRepository<User, Integer> {
-
-    User findByUserName(String username);
-}
--- BackEnd/spring-boot-data-H2-embedded/src/main/resources/schema-h2.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user
-(
-    id int(10) identity NOT NULL,
-    user_name VARCHAR2(50) NOT NULL,
-    PRIMARY KEY(id),
-    pass_word varchar2(50) not null
-);
