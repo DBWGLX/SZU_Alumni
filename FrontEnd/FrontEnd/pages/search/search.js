581851af579a4029198e935ac3709dea2613c3da
balaa <2941930242@qq.com>
2024-12-09 18:35:17 +0800

    UI: beautify the UI

--- FrontEnd/FrontEnd/pages/search/search.js
+++ FrontEnd/FrontEnd/pages/search/search.js
@@ -1,12 +1,14 @@
 Page({
   data: {
     originalList: [
-      { id: 1, name: "张三", class: "2020届", graduationYear: "2020", imagePath: "https://picsum.photos/seed/picsum/200/300", gender: 0, department: "计算机与软件学院", city: "深圳市" },
-      { id: 2, name: "李四", class: "2019届", graduationYear: "2019", imagePath: "https://picsum.photos/seed/picsum/200/300", gender: 0, department: "电子与信息工程学院", city: "广州市"},
-      { id: 3, name: "樊顺琪", class: "2022届", graduationYear: "2022", imagePath: "https://picsum.photos/seed/picsum/200/300", gender: 0,  department: "计算机与软件学院", city: "深圳市"  }
+      { id: 1, name: "张三", class: "2020届", graduationYear: "2020", imagePath: "/images/wx_1.jpg", gender: 0, department: "计算机与软件学院", city: "深圳市" },
+      { id: 2, name: "李四", class: "2019届", graduationYear: "2019", imagePath: "/images/wx_1.jpg", gender: 0, department: "电子与信息工程学院", city: "广州市"},
+      { id: 3, name: "樊顺琪", class: "2022届", graduationYear: "2022", imagePath: "/images/wx_1.jpg", gender: 0,  department: "计算机与软件学院", city: "深圳市"  }
     ],
     alumniList: [],
-    searchQuery: ''
+    searchQuery: '',
+    categories: ['全部分类', '计算机', '金融'],
+    selectedCategory: '全部分类',
   },
   onLoad() {
     this.setData({ alumniList: this.data.originalList }); // 初始化时显示所有校友
