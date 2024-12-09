551309e024bac21a263c963f6739181c1b624036
DOTPHTP <1423701360@qq.com>
2024-12-09 22:45:20 +0800

    修改图片编码相关代码，以及增加了错误储存时删除失败文件

--- BackEnd/Backend_Discuss/test.py
+++ BackEnd/Backend_Discuss/test.py
@@ -10,7 +10,7 @@ post_data = {
     "time": "2023-10-10T10:10:09",
     "detailTop": "发帖标题",
     "detailAll": "发帖内容正文",
-    "dePic": ["1234"]
+    "dePic": ["data:image/jepg;Base64,1234"]
 }
 response = requests.post(f"{base_url}/list", json=post_data)
 print("插入动态回复:")
@@ -26,7 +26,7 @@ search_data = {
     "begin": 0,
     "number": 10
 }
-response = requests.get(f"{base_url}/list", json=search_data)
+response = requests.get(f"{base_url}/list", params=search_data)
 print("查询动态回复:")
 print(response.json())
 
@@ -35,7 +35,7 @@ search_by_id_data = {
     "id": moment_id,
     "time": "2023-10-10T10:10:14"
 }
-response = requests.get(f"{base_url}/list/text", json=search_by_id_data)
+response = requests.get(f"{base_url}/list/text", params=search_by_id_data)
 print("根据ID查询具体的帖子正文回复:")
 print(response.json())
 
@@ -44,22 +44,22 @@ search_by_user_data = {
     "id": "2",
     "time": "2023-10-10T10:10:15"
 }
-response = requests.get(f"{base_url}/list/byuser", json=search_by_user_data)
+response = requests.get(f"{base_url}/list/byuser", params=search_by_user_data)
 print("根据用户ID查询动态回复:")
 print(response.json())
 
 # 随机获取动态
-response = requests.get(f"{base_url}/list/random",json ={"num":5})
+response = requests.get(f"{base_url}/list/random",params ={"num":5})
 print ("随机获取动态")
 print(response.json())
 
 # 删除动态
-delete_data = {
-    "id": "2",
-    "time": "2023-10-10T10:10:17",
-    "disId": moment_id
-}
-response = requests.delete(f"{base_url}/list", json=delete_data)
-print("删除动态回复:")
-print(response.json())
+# delete_data = {
+#     "id": "2",
+#     "time": "2023-10-10T10:10:17",
+#     "disId": moment_id
+# }
+# response = requests.delete(f"{base_url}/list", json=delete_data)
+# print("删除动态回复:")
+# print(response.json())
 
