052d0cff4646401cf34b7767a01713e3a489d4f2
a0trox <145950871+a0trox@users.noreply.github.com>
2024-12-20 17:23:53 +0800

    Delete BackEnd/Backend_Login directory

--- BackEnd/Backend_Login/src/main/java/com/example/backend_login/repository/UserContactsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend_login.repository;
-
-import com.example.backend_login.entity.UserContacts;
-import com.example.backend_login.entity.UserPrivacy;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserContactsRepository extends CrudRepository<UserContacts,Integer> {
-}
